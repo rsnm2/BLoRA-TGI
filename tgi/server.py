c5138e0db6b0f66d748793066ca5c5648ed131ac
rsnm2 <rshaw@neuralmagic.com>
2023-09-12 21:21:01 +0000

    adding sse

--- tgi/server.py
+++ tgi/server.py
@@ -1,9 +1,12 @@
 import fastapi, uvicorn
+from sse_starlette.sse import EventSourceResponse
 from contextlib import asynccontextmanager
 
 from threading import Thread
 from router import TextGenerationRouter, batching_task
 from utils import GenerateRequestInputs, GenerateRequestOutputs, GenerateRequest
+
+MESSAGE_STREAM_RETRY_TIMEOUT = 15000  # milisecond
 
 base_model_id = "decapoda-research/llama-7b-hf"
 lora_ids = ["jondurbin/airoboros-7b-gpt4-1.2-peft", "trl-lib/llama-7b-se-rl-peft", 'winddude/wizardLM-LlaMA-LoRA-7B']
@@ -55,11 +58,28 @@ def generate(inputs: GenerateRequestInputs) -> GenerateRequestOutputs:
         finish_reason=generation.finish_reason
     )
 
+@app.post("/generate_stream")
+async def generate_stream_stream(request: fastapi.Request, inputs: GenerateRequestInputs):
+    # convert input to generate request, submit to router
+    generate_request = GenerateRequest.from_gr_inputs(inputs)
+    artifacts["router"].submit_request(generate_request)
+
+    async def token_generator():
+        generation = generate_request.response_stream.get()    
+        
+        while not generation.stopped:
+            if await request.is_disconnected():
+                break
+
+            yield artifacts["router"].service.model.tokenizer.decode(generation.token_id)
+            generation = generate_request.response_stream.get()
+
+    return EventSourceResponse(token_generator())
+
 if __name__ == "__main__":
     uvicorn.run(
         "server:app",
         host="0.0.0.0",
         port=5543,
         workers=1,      # limit to one process to avoid copying the model
-        # reload=True
     )
